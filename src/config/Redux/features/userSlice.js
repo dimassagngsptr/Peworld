0667a7e7b9931b974f363f36613cb617331e3425
Dimas Ageng Saputro <dimasageng58@gmail.com>
2024-04-19 15:53:39 +0700

    fix: userSlice

--- src/config/Redux/features/userSlice.js
+++ src/config/Redux/features/userSlice.js
@@ -2,57 +2,56 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { getApi } from "../../../utils/get/get";
 
 export const getActiveUser = createAsyncThunk(
-  "user/getActiveUser",
-  async (role, { getState }) => {
-    const state = getState().user;
-    if (state.loading !== "pending") {
+   "user/getActiveUser",
+   async (role, thunkApi) => {
+      const { user } = thunkApi.getState();
+      /*if (state.loading !== "pending") {
       return;
-    }
-    const response = await getApi(`${role}/profile`);
-    return response;
-  }
+    }*/
+      const response = await getApi(`${role}/profile`);
+      return response;
+   }
 );
-
+//post masukan kedalam parameter 
 const userSlice = createSlice({
-  name: "user",
-  initialState: {
-    loading: "idle",
-    activeUser: [],
-    currentRequestId: undefined,
-  },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(getActiveUser.pending, (state, action) => {
-        if (state.loading === "idle") {
-          state.loading = "pending";
-          state.currentRequestId = action.meta.requestId;
-        }
-      })
-      .addCase(getActiveUser.fulfilled, (state, action) => {
-        const { requestId } = action.meta;
-        if (
-          state.loading === "pending" &&
-          state.currentRequestId === requestId
-        ) {
-          state.loading = "idle";
-          state.activeUser = action.payload;
-          console.log(action);
-          state.currentRequestId = undefined;
-        }
-      })
-      .addCase(getActiveUser.rejected, (state, action) => {
-        const { requestId } = action.meta;
-        if (
-          state.loading === "pending" &&
-          state.currentRequestId === requestId
-        ) {
-          state.loading = "idle";
-          state.error = action.error;
-          state.currentRequestId = undefined;
-        }
-      });
-  },
+   name: "user",
+   initialState: {
+      loading: "idle",
+      activeUser: [],
+      currentRequestId: undefined,
+   },
+   reducers: {},
+   extraReducers: (builder) => {
+      builder
+         .addCase(getActiveUser.pending, (state, action) => {
+            if (state.loading === "idle") {
+               state.loading = "pending";
+               state.currentRequestId = action.meta.requestId;
+            }
+         })
+         .addCase(getActiveUser.fulfilled, (state, action) => {
+            const { requestId } = action.meta;
+            if (
+               state.loading === "pending" &&
+               state.currentRequestId === requestId
+            ) {
+               state.loading = "idle";
+               state.activeUser = action.payload;
+               state.currentRequestId = undefined;
+            }
+         })
+         .addCase(getActiveUser.rejected, (state, action) => {
+            const { requestId } = action.meta;
+            if (
+               state.loading === "pending" &&
+               state.currentRequestId === requestId
+            ) {
+               state.loading = "idle";
+               state.error = action.error;
+               state.currentRequestId = undefined;
+            }
+         });
+   },
 });
 
 export default userSlice.reducer;
